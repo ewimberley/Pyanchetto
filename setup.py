3441c621272af15efa79abb66a03de5758cca775
Eric Wimberley <blank89@gmail.com>
2019-11-21 20:38:02 -0500

    Forgot some files in commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='deepfianchetto',
+      version='0.1',
+      description='A python chess engine for deep learning experimenation.',
+      author='Eric Wimberley',
+      packages=['deepfianchetto'],
+     )
